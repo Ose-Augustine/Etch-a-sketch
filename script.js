38a1d4647830caf4a652f6a9ae2cf24fc0b558f3
Ose-Augustine <osebeckley@gmail.com>
2022-03-25 15:15:45 +0100

    Added padding to the boxes to give them defined shapes
    Previous method only takes squre form if it contains a text

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container=document.querySelector("#container");
+const button=document.createElement("button");
+button.textContent="Clear current grid";
+container.appendChild(button);
 i=0
 const subContainer=document.createElement("div");
 subContainer.classList.toggle("subContainer")
 container.appendChild(subContainer);
 while(i < 100){
     let newDiv=document.createElement("div");
-    newDiv.textContent="hello world";
+    newDiv.textContent=" ";
     subContainer.appendChild(newDiv);
-
     i ++;
 }
-//subContainer.style.display="flex";
+button.addEventListener("click",function(){
+    alert(prompt('Number of square per side of the new grid?',''));
+})
