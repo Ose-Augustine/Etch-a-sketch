d7234efbba154b04f3d35adcd5bd90f728a46446
Ose-Augustine <osebeckley@gmail.com>
2022-03-26 05:38:55 +0100

    Added a userStyle class to handle user grid choice
     Specifics for class modifications added to the generator function
    Updated css to include a grid template for rows( this worked for square layout

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
 const container=document.querySelector("#container");
-const button=document.createElement("button");
-button.textContent="Clear current grid";
-container.appendChild(button);
-i=0
+//first button and appends to the container//
+const button0=document.createElement("button");
+button0.textContent="Generate your design";
+container.appendChild(button0);
+
+//second button, adds class "button1" and appends to container//
+const button1=document.createElement("button");
+button1.textContent="generate default grid";
+button1.classList.toggle("button1");
+container.appendChild(button1);
+
+//subContainer to handle all the square divs//
 const subContainer=document.createElement("div");
 subContainer.classList.toggle("subContainer")
 container.appendChild(subContainer);
-originalLayOut();
+
+//subContainer to handle all the user style grid//
+const userStyle=document.createElement("div");
+userStyle.classList.toggle("userStyle");
+container.appendChild(userStyle);
+userStyle.style.display='grid';
 
 function originalLayOut(){
-    while(i < 100){
+    i=0;
+    while(i < 64){
         let newDiv=document.createElement("div");
         newDiv.textContent=" ";
         subContainer.appendChild(newDiv);
         i ++;
-}}
-
-let userInput=' ';
-
-button.addEventListener("click",function(){
-    let userChoice=prompt('Number of square per side of the new grid?','')
-    userInput += userChoice;
-    alert(`Changing the layout to your liking with ${userChoice}`);
-})
-console.log(userInput);
-
+    };
+}
 function generateUserGrid(userInput){
-    i=0;
-    while(i < userInput){
+    let gridLimit=userInput ** 2;
+    userStyle.setAttribute('style',`grid-template-columns:repeat(${userInput},1fr)`);
+    userStyle.setAttribute('style',`grid-template-rows:repeat (${userInput},1fr)`);
+    i=0;    
+    while((i < gridLimit) && userInput <= 100){
         let userGridSpec=document.createElement('div');
         userGridSpec.textContent=' ';
-        subContainer.appendChild(userGridSpec);
+        userStyle.appendChild(userGridSpec);
         i ++;
     }
 }
+
+let userInput=' ';
+
+button0.addEventListener("click",function(){
+    console.clear();
+    let userChoice=prompt('Number of square per side of the new grid?','')
+    generateUserGrid(userChoice);
+    userInput += userChoice;
+    alert(`Changing the layout to your liking with ${userChoice}`);
+})
+button1.addEventListener('click',function(){
+    originalLayOut();
+    alert("This is the original layout of this page. Try making yours!\n Refresh before changing!")
+})
+
+
